f87849b401f50cf75ce543b9606bf1988bbff565
13um45 <christian.leumas@icloud.com>
2016-02-15 04:09:32 -0500

    finished login feature.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User@user = User.
+      find_by(username: params[:username].downcase).
+      try(:authenticate, params[:password])
+
+    if @user
+      session[:user_id] = @user.id
+      redirect_to products_path
+    else
+      render action: 'new'
+    end
+  end
 end
