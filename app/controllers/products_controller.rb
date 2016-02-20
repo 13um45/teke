3196f2a31326a3c57caf1422f276cfd422467a3b
13um45 <christian.leumas@icloud.com>
2016-02-20 03:31:23 -0500

    added active to product params.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -66,6 +66,6 @@ class ProductsController < ApplicationController
 
 private
   def product_params
-    params.require(:product).permit(:name, :description, :price, :quantity)
+    params.require(:product).permit(:name, :description, :price, :quantity, :active)
   end
 end
