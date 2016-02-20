c7a76891abef64fbc198dada600797d57b912609
13um45 <christian.leumas@icloud.com>
2016-02-20 03:57:36 -0500

    removed add cart options from index page. added add to cart option to product page.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,6 +27,7 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     @images = @product.image
     @charge = @product.charges.new
+    @order_item = current_order.order_items.new
   end
 
   def new
