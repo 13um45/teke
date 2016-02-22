7cee2502d9b58b92c416179c34be07f275226c07
13um45 <christian.leumas@icloud.com>
2016-02-22 05:19:08 -0500

    added more styling to charges index. added red highlight to charges that need attention. added navbar. added styling to product form.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,8 @@ class Product < ActiveRecord::Base
 
   has_many :order_items
   has_one :image, dependent: :destroy
-  validates :name, :description, :presence => true, :length => { :minimum => 2 }
-  validates :price, :quantity, :presence => true, numericality: { only_integer: true }
+  validates :name, :description, :length => { :minimum => 2 }
+  validates :price, :quantity, numericality: { only_integer: true }
 
   default_scope { where(active: true) }
 end
